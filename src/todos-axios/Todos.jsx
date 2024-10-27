f300e323f0725c4aec43f63dc445ada6bee8c8e9
Alex <phoenix.trade2307@gmail.com>
2024-10-27 21:14:34 +0200

    async await for todos from jsonplaceholder

--- src/todos-axios/Todos.jsx
+++ src/todos-axios/Todos.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 
 const api = {
     getTodo() {
+
         return axios.get('https://jsonplaceholder.typicode.com/todos')
             .then(res => res.data)
     }
@@ -10,22 +11,50 @@ const api = {
 
 export const Todos = () => {
 
+    const [error, setError] = useState(null)
     const [todos, setTodos] = useState([])
-    const onClickLoad = () => {
-        api.getTodo().then((data) => {
-            console.log(todos)
-            setTodos([todos, ...data])
-        })
+    const [loading, setLoading] = useState(false)
 
+
+    // // with then
+    // const onClickLoad = () => {
+    //     setLoading(true)
+    //     api.getTodo()
+    //         .then((data) => {
+    //             setTodos([todos, ...data])
+    //         })
+    //         .catch(error => {
+    //             setError(`${error}`)
+    //         })
+    //         .finally(() => setLoading(false))
+    // }
+    //
+    // const onClickClear = () => {
+    //     setTodos([])
+    // }
+
+    // with async/await
+    const onClickLoad = async () => {
+        setLoading(true)
+        try {
+
+            const data = await api.getTodo()
+            setTodos([todos, ...data])
+        } catch (err) {
+            setError(`${err}`)
+        } finally {
+            setLoading(false)
+        }
     }
+
     const onClickClear = () => {
         setTodos([])
     }
 
-
-    return (
+    return loading ? (<div>Loading...</div>) : (
         <div>
             <button onClick={onClickLoad}>load todolist</button>
+            {error && <div>{error}</div>}
             <ul>
                 {todos.map(t =>
                     <li key={t.id}><span>{t.id}</span> {t.title}</li>
